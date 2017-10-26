589edda721cad917bc8d128b800b23866f802671
WinstonKamau <crispus.kamau@andela.com>
2017-10-26 07:30:12 +0300

    (stlye(eslint): Change code based on lint
    Tab statements properly
    Reduce length of long statements
    
    [Fixes #Challenge4]

--- src/ActivityListComponent.js
+++ src/ActivityListComponent.js
@@ -24,7 +24,14 @@ export class ActivityTable extends Component {
                 <td>{row.activity_name}</td>
                 <td>
                   <center>
-                    <Button type="submit" onClick={this.props.open} className="btn btn-default btn-sm glyphicon glyphicon-pencil" name="editActivityButton" id={row.activity_name} value={row.id} />
+                    <Button
+                      type="submit"
+                      onClick={this.props.open}
+                      className="btn btn-default btn-sm glyphicon glyphicon-pencil"
+                      name="editActivityButton"
+                      id={row.activity_name}
+                      value={row.id}
+                    />
                   </center>
                 </td>
                 <td>
@@ -55,9 +62,9 @@ export class CreateActivityModal extends Component {
         <form onSubmit={this.props.handleSubmit} name="createActivityForm">
           <Modal.Header>
             <Modal.Title>Create a new Activity</Modal.Title>
-                        
+
           </Modal.Header>
-            
+
           <Modal.Body>
             <FormGroup validationState={this.props.createStatusActivityForm}>
               <HelpBlock>{this.props.createActivityError}</HelpBlock>
@@ -65,64 +72,66 @@ export class CreateActivityModal extends Component {
               <input name="activity_name" type="text" onChange={this.props.handleChange} required/>
             </FormGroup>
           </Modal.Body>
-            
+
           <Modal.Footer>
             <Button name="closeActivityButton" onClick={this.props.close}>Close</Button>
             <Button type="submit" bsStyle="primary">Save changes</Button>
           </Modal.Footer>
         </form>
       </Modal>
-        );
-        
-    }
+    );
+  }
 }
-export class EditActivityModal extends Component{
-    render(){
-        return(
-            <Modal show={this.props.showEditActivityModal} onHide={this.props.close}>
-                <form onSubmit={this.props.handleSubmit} name="editActivityForm">
-                    <Modal.Header>
-                    <Modal.Title>Edit Activity {this.props.previousActivityName}</Modal.Title>
-                        
-                    </Modal.Header>
-            
-                    <Modal.Body>
-                        <FormGroup validationState={this.props.editStatusActivityForm}>
-                            <HelpBlock>{this.props.editActivityError}</HelpBlock>
-                            <ControlLabel >New Activity Name </ControlLabel>
-                            <input name="editActivityName" type="text" onChange={this.props.handleChange} required/>
-                        </FormGroup>
-                    </Modal.Body>
-            
-                    <Modal.Footer>
-                    <Button name="closeEditActivityButton" onClick={this.props.close}>Close</Button>
-                    <Button type="submit" bsStyle="primary">Save changes</Button>
-                    </Modal.Footer>
-                </form>
-        </Modal>
-        );
-    }
+export class EditActivityModal extends Component {
+  render() {
+    return (
+      <Modal show={this.props.showEditActivityModal} onHide={this.props.close}>
+        <form onSubmit={this.props.handleSubmit} name="editActivityForm">
+          <Modal.Header>
+            <Modal.Title>Edit Activity {this.props.previousActivityName}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormGroup validationState={this.props.editStatusActivityForm}>
+              <HelpBlock>{this.props.editActivityError}</HelpBlock>
+              <ControlLabel >New Activity Name </ControlLabel>
+              <input
+                name="editActivityName"
+                type="text"
+                onChange={this.props.handleChange}
+                required
+              />
+            </FormGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button name="closeEditActivityButton" onClick={this.props.close}>Close</Button>
+            <Button type="submit" bsStyle="primary">Save changes</Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    );
+  }
 }
 
-export class DeleteActivityModal extends Component{
-    render(){
-        return(
-        <Modal show={this.props.showDeleteActivityModal} onHide={this.props.close}>
-            <form onSubmit={this.props.handleSubmit} name="deleteActivityForm">
-                <Modal.Header>
-                <Modal.Title>Delete Activity</Modal.Title>
-                </Modal.Header>
-        
-                <Modal.Body>
-                Are you sure you want to delete the activity <strong>{this.props.deletableActivityName}</strong>?
-                </Modal.Body>
-        
-                <Modal.Footer>
-                <Button name="closeDeleteActivityButton" onClick={this.props.close}>Close</Button>
-                <Button type="submit" className="btn btn-danger" bsStyle="primary">Remove</Button>
-                </Modal.Footer>
-            </form>
-        </Modal>
-        );
-    }
+export class DeleteActivityModal extends Component {
+  render() {
+    return (
+      <Modal show={this.props.showDeleteActivityModal} onHide={this.props.close}>
+        <form onSubmit={this.props.handleSubmit} name="deleteActivityForm">
+          <Modal.Header>
+            <Modal.Title>Delete Activity</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            Are you sure you want to delete the activity
+            <strong>{this.props.deletableActivityName}</strong>?
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button name="closeDeleteActivityButton" onClick={this.props.close}>Close</Button>
+            <Button type="submit" className="btn btn-danger" bsStyle="primary">Remove</Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    );
+  }
 }
